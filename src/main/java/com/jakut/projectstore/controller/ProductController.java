0c5b751a538d48b92f6c7548b291b7f5db0c833e
gintariukai <gintariukai@hotmail.com>
2023-03-22 18:38:41 +0200

    1. first REST API controller Added RestController, RequestController and Data, Builder, AllArgsConstructor, NoArgsConstructor

--- src/main/java/com/jakut/projectstore/controller/ProductController.java
+++ src/main/java/com/jakut/projectstore/controller/ProductController.java
@@ -1,13 +1,28 @@
 package com.jakut.projectstore.controller;
 
 
+import com.jakut.projectstore.dto.ProductDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController //funkcija kuri grazins saraso laukeli
+@RequestMapping("products")
 public class ProductController {
 
+    @GetMapping
     public ResponseEntity<List<ProductDto>> getProducts() {
+        ProductDto product = ProductDto.builder()
+                .title("Pirmas produktas")
+                .build();
+
+        List<ProductDto> products = new ArrayList<>();
+        products.add(product);
+        return ResponseEntity.ok(products);
 
     }
 }
